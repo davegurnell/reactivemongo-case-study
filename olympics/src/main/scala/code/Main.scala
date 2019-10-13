08238ea48074bf326e9886af9a326ddcc5e8399e
Dave Gurnell <dave@cartographer.io>
2019-10-13 10:07:01 +0100

    Exercises

--- olympics/src/main/scala/code/Main.scala
+++ olympics/src/main/scala/code/Main.scala
@@ -27,33 +27,10 @@ object Main {
     values
   }
 
-  def medalBSON: Iterator[BSONDocument] =
-    rawMedalData.map { data =>
-      BSONDocument(data.map {
-        case (key, value) =>
-          key -> BSONString(value)
-      })
-    }
-
-  val driver = MongoDriver()
-
-  def openDatabase: Future[DefaultDB] =
-    for {
-      uri  <- Future.fromTry(MongoConnection.parseURI(s"mongodb://localhost:27017"))
-      conn <- Future.fromTry(driver.connection(uri, strictUri = true))
-      db   <- conn.database("olympics")
-    } yield db
-
-  def program: Future[Unit] =
-    for {
-      db <- openDatabase
-      _  <- Future.traverse(rawMedalData) { doc =>
-              println(s"Inserting $doc")
-              db.collection[BSONCollection]("medals").insert.one(doc)
-            }
-    } yield ()
+  // Insert most of you app here...
 
   def main(args: Array[String]): Unit = {
-    Await.result(program, 10.seconds)
+    // Insert a single line to kick things off here...
+    println("TODO")
   }
 }
