39fe516ba4cda52df2a690b5a9a779416e1a75c2
Dave Gurnell <dave@cartographer.io>
2019-10-14 15:09:03 +0100

    Recreate resource exhaustion bug

--- olympics/src/main/scala/code/Main.scala
+++ olympics/src/main/scala/code/Main.scala
@@ -2,12 +2,12 @@ package code
 
 import com.github.tototoshi.csv._
 import reactivemongo.api.collections.bson.BSONCollection
-import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
+import reactivemongo.api.{MongoConnection, MongoDriver}
 import reactivemongo.bson.{BSONDocument, BSONString}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.{BufferedSource, Source}
 
 object Main {
@@ -37,23 +37,25 @@ object Main {
 
   val driver = MongoDriver()
 
-  def openDatabase: Future[DefaultDB] =
+  def collection: Future[BSONCollection] =
     for {
       uri  <- Future.fromTry(MongoConnection.parseURI(s"mongodb://localhost:27017"))
       conn <- Future.fromTry(driver.connection(uri, strictUri = true))
       db   <- conn.database("olympics")
-    } yield db
+    } yield db.collection[BSONCollection]("medals")
 
   def program: Future[Unit] =
     for {
-      db <- openDatabase
-      _  <- Future.traverse(rawMedalData) { doc =>
-              println(s"Inserting $doc")
-              db.collection[BSONCollection]("medals").insert.one(doc)
-            }
+      coll <- collection
+      // This fails with repeated exceptions:
+      //     'No active channel can be found to the primary node:
+      //         'localhost:27017' { connected:1, channels:2 } (Supervisor-1/Connection-1)'
+      _    <- Future.traverse(rawMedalData)(doc => coll.insert.one(doc))
+      // This does not:
+      // _    <- coll.insert.many(rawMedalData.toIterable)
     } yield ()
 
   def main(args: Array[String]): Unit = {
-    Await.result(program, 10.seconds)
+    Await.result(program, 60.seconds)
   }
 }
